cd1e1278702e5c6b51405798905db71aaf975a9d
Timothy Liao <s446424@sinyi.com.tw>
2022-12-11 20:45:59 +0800

    feat: init openai

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai');
 const dotenv = require('dotenv');
+const _ = require('lodash');
 
 const logger = require('./lib/Logger');
 
+// config
 dotenv.config();
+const { BOT_TOKEN, OPENAI_TOKEN } = process.env;
+const config = new Configuration({
+  apiKey: OPENAI_TOKEN,
+});
 
-const { BOT_TOKEN } = process.env;
+const openai = new OpenAIApi(config);
 
 // 创建 Discord Client 实例
 const client = new Client({
@@ -16,18 +23,44 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
   ],
 });
-// 当客户端准备就绪时，输出 "Bot is ready!"
+
 client.on('ready', () => {
   logger.debug({ msg: 'Bot is ready!' });
 });
 
-// 当接收到新消息时，检查消息内容是否为 "ping"，如果是，则向消息发送者回复 "pong"
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
   logger.debug({ nsg: 'got message', message });
+  if (message.member.user.bot) return;
+
+  const firstSplitWord = _.chain(message.content).split(' ').first().value();
+  const isCommand = _.startsWith(firstSplitWord, '-');
+
   if (message.content === 'ping') {
     message.channel.send('https://tenor.com/view/pong-gif-26462133');
   }
+
+  // command
+  if (!isCommand) return;
+  const trimFirstSplitWord = _.trimStart(firstSplitWord, '-');
+  if (trimFirstSplitWord === 'talk') {
+    message.channel.send('talk back ok');
+  }
+
+  if (trimFirstSplitWord === 'ai') {
+    const input = _.chain(message.content).split(' ').last().value();
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: input,
+      temperature: 0.9,
+      max_tokens: 1000,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: ['AI', 'HUMAN'],
+    });
+
+    const response = _.get(completion, 'data.choices.0.text');
+    message.channel.send(response);
+  }
 });
 
-// 登录 Discord 服务器，开始接收和处理消息
 client.login(BOT_TOKEN);
